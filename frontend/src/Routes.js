2da774e616358e5568822781cb3a730879c838d6
vaspoz <vaspoz@mail.ru>
2022-06-16 16:38:29 +0200

    Added billing and notes management pages

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,6 +4,9 @@ import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 
 export default function Links() {
   return (
@@ -11,6 +14,9 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
+      <Route path="/notes/:id" element={<Notes />} />
+      <Route path="/settings" element={<Settings />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
     </Routes>
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,19 +1,22 @@
 const config = {
-    // Backend config
-    s3: {
-      REGION: process.env.REACT_APP_REGION,
-      BUCKET: process.env.REACT_APP_BUCKET,
-    },
-    apiGateway: {
-      REGION: process.env.REACT_APP_REGION,
-      URL: process.env.REACT_APP_API_URL,
-    },
-    cognito: {
-      REGION: process.env.REACT_APP_REGION,
-      USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
-      APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
-      IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
-    },
-  };
-  
-  export default config;
+  // Backend config
+  s3: {
+    REGION: process.env.REACT_APP_REGION,
+    BUCKET: process.env.REACT_APP_BUCKET,
+  },
+  apiGateway: {
+    REGION: process.env.REACT_APP_REGION,
+    URL: process.env.REACT_APP_API_URL,
+  },
+  cognito: {
+    REGION: process.env.REACT_APP_REGION,
+    USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY:
+    "pk_test_51I3POiJJCaz63LZXNTUlBogJ7on1YYsLm2zIOQQvF8cUnDNcI59vFBU7i4km8S5IuX8AwrVIFhhadhsYw77nl9pf00pC7d6Y8b",
+};
+
+export default config;
