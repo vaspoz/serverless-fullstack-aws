75984edd215a274915fe49e84ff9f4fd1abc5957
vaspoz <vaspoz@mail.ru>
2022-06-16 15:11:00 +0200

    Added signin/up/out user flows

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,16 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Links() {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        {
-            /* Finally, catch all unmatched routes */
-        }
-        <Route path="*" element={<NotFound />} />;
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      {/* Finally, catch all unmatched routes */}
+      <Route path="*" element={<NotFound />} />;
     </Routes>
   );
 }
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./Login.css";
 import { Auth } from "aws-amplify";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import LoaderButton from "../components/LoaderButton";
 import { useAppContext } from "../lib/contextLib";
+import { useFormFields } from "../lib/hooksLib";
+import { onError } from "../lib/errorLib";
+import "./Login.css";
 
 export default function Login() {
+  const nav = useNavigate();
   const { userHasAuthenticated } = useAppContext();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
 
+    setIsLoading(true);
+
     try {
-        await Auth.signIn(email, password);
-        alert("Logged in");
-        userHasAuthenticated(true);
-      } catch (e) {
-        alert(e.message);
-      }
+      await Auth.signIn(fields.email, fields.password);
+      userHasAuthenticated(true);
+      nav("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -34,21 +44,27 @@ export default function Login() {
           <Form.Control
             autoFocus
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </Form.Group>
-        <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
+        <LoaderButton
+          block="true"
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </Form>
     </div>
   );
