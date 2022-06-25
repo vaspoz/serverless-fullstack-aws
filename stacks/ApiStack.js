999cc2714ac5b3b4eba3a673a9e63799034bb7ab
vaspoz <vaspoz@mail.ru>
2022-06-25 20:44:46 +0200

    Added a seed build spec

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -1,37 +1,35 @@
-import { Api, use } from "@serverless-stack/resources"
-import { StorageStack } from "./StorageStack"
+import { Api, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
 
-export const ApiStack = ({stack, app}) => {
+export const ApiStack = ({ stack, app }) => {
+  let { table } = use(StorageStack);
 
-    let {table} = use(StorageStack);
-
-    let api = new Api(stack, 'Api', {
-        defaults: {
-            authorizer: 'iam',
-            function: {
-                permissions: [table],
-                environment: {
-                    TABLE_NAME: table.tableName,
-                    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-                }
-            }
+  let api = new Api(stack, "Api", {
+    defaults: {
+      authorizer: "iam",
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+          STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
         },
-        routes: {
-            "POST /notes": "functions/create.main",
-            "GET /notes/{id}": "functions/get.main",
-            "GET /notes": "functions/list.main",
-            "PUT /notes/{id}": "functions/update.main",
-            "DELETE /notes/{id}": "functions/delete.main",
-            "POST /billing": "functions/billing.main",
-        }
-    });
+      },
+    },
+    routes: {
+      "POST /notes": "functions/create.main",
+      "GET /notes/{id}": "functions/get.main",
+      "GET /notes": "functions/list.main",
+      "PUT /notes/{id}": "functions/update.main",
+      "DELETE /notes/{id}": "functions/delete.main",
+      "POST /billing": "functions/billing.main",
+    },
+  });
 
-    stack.addOutputs({
-        ApiEndpoint: api.url
-    });
+  stack.addOutputs({
+    ApiEndpoint: api.customDomainUrl || api.url,
+  });
 
-    return {
-        api
-    }
-
-}
+  return {
+    api,
+  };
+};
